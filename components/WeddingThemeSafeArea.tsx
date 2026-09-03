6016caa46be88309635c1a6ecddfbcac7c1812a6
vistiqstudio-invitation <vistiqstudio@gmail.com>
2026-09-03 19:02:33 +0700

    Fix shared frame date typing

--- components/WeddingThemeSafeArea.tsx
+++ components/WeddingThemeSafeArea.tsx
@@ -44,6 +44,10 @@ function frameDate(invitation?: InvitationFrameData) {
     return invitation.events[0]?.date || "";
   }
 
+  if (invitation.category === "aqiqah") {
+    return invitation.event?.date || invitation.baby.birthDate || "";
+  }
+
   return invitation.event?.date || invitation.child.birthDate || "";
 }
 
